9713e78a0470b6f56811993488ece0ca7cfbd45c
Manoel Almeida <manoel.almeida@sptech.school>
2023-03-26 14:55:18 -0300

    Adding template selector layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from 'react';
+
 import Document from './components/Document';
 import documentService from '../../service/DocumentService';
 
+import styles from './Home.module.css';
+import Template from './components/Template';
+
 function Home() {
   const [documents, setDocuments] = useState([]);
+
+  const templates = [
+    {
+      id: 1,
+      name: 'Blank document',
+    },
+  ];
 
   useEffect(() => {
     documentService.listAll({
@@ -14,8 +25,16 @@ function Home() {
   }, []);
 
   return (
-    <div style={{ padding: 50 }}>
-      {documents.map((item) => (<Document key={item.id} document={item} />))}
+    <div>
+      <div className={`${styles.templates} ${styles.container}`}>
+        <span className={styles.title}>Create</span>
+        <div style={{ display: 'flex', margin: '25px 0 0 0' }}>
+          {templates.map((item) => (<Template key={item.id} template={item} />))}
+        </div>
+      </div>
+      <div className={styles.container}>
+        {documents.map((item) => (<Document key={item.id} document={item} />))}
+      </div>
     </div>
   );
 }
